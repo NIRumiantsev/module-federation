f127db088e7c44278380257175797667d0756b10
@nrumyantsev <nrumyantsev@phoenixit.ru>
2021-08-27 17:43:26 +0300

    Create initial project files. Add MF config to the host and marketing app

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MarketingApp } from './apps/MarketingApp';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+      <MarketingApp/>
+    </div>
+  )
+};
+
+export { App };
