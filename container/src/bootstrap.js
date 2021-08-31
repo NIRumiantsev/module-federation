c464e159a6d80481c3e6cb083870517a8a111661
@nrumyantsev <nrumyantsev@phoenixit.ru>
2021-08-31 13:55:53 +0300

    Fix container app

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,15 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App/>, el);
-};
-
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#root');
-  if (devRoot) {
-    mount(devRoot);
-  }
-}
-
-export { mount };
+ReactDOM.render(<App />, document.querySelector('#root'));
